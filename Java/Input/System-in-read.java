03a93541a0ef6634a52714986e2a6aaf6dca7a3d
spacy-owl <130344606+spacy-owl@users.noreply.github.com>
2024-03-13 15:18:55 +0900

    Update System-in-read.java

--- Java/Input/System-in-read.java
+++ Java/Input/System-in-read.java
@@ -10,7 +10,7 @@ public class Main {
 
     static int read() throws IOException {
         int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        while ((c = System.in.read()) > 47 && c < 58) n = (n << 3) + (n << 1) + (c & 15);
         return n;
     }
 }
